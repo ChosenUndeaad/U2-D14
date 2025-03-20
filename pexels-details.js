7c65cf9d009586a32397691ef1b3df4fe29e2252
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-03-20 15:58:58 +0100

    Update

---
+++ pexels-details.js
@@ -0,0 +1,20 @@
+const imgDetail = (photoId) => {
+  const url = `https://api.pexels.com/v1/photos/${photoId}`;
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: apiKey,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Errore HTTP");
+      }
+    })
+    .catch((error) =>
+      console.error("Errore nel caricamento dei dettagli dell'immagine:", error)
+    );
+};
